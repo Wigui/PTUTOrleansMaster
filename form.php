a6573e5c298ef5908ac9c5fa1866e422f163d5b4
Hordorr <1998glesage@gmail.com>
2018-11-23 09:41:25 +0100

    Agrandissement du form

--- form.php
+++ form.php
@@ -14,6 +14,11 @@ function connectV1(){
 	Nom: <input type="txt" name="nom" value=""/></br>
 	Prénom: <input type="txt" name="prenom" value=""/></br>
 	Club: <input type="txt" name="club" value=""/></br>
+  P1: <input type="txt" name="poste1" value="Serveur"/></br>
+  P2: <input type="txt" name="poste2" value="Danseur"/></br>
+  P3: <input type="txt" name="poste3" value="Music"/></br>
+  Jd: <input type="txt" name="joursDispo" value="Lundi"/></br>
+  Hd: <input type="txt" name="heuresDispo" value="16h"/></br>
 	<input type="submit" name="valider" value="Valider">
 </form>
 <?php
@@ -25,8 +30,6 @@ catch (Exception $e)
         die('Erreur : ' . $e->getMessage());
 }
 $incomplet=empty($nom)||empty($prenom)||empty($club);
-
-
 if (isset($_POST['valider']))
 {
 $erreur=false;
@@ -45,19 +48,47 @@ if(empty($_POST['nom']))
     //erreur au niveau du message
     $erreur= true;
   }
+  if(empty($_POST['poste1']))
+  {
+    //erreur au niveau du message
+    $erreur= true;
+  }  
+  if(empty($_POST['joursDispo']))
+  {
+    //erreur au niveau du message
+    $erreur= true;
+  }
+  if(empty($_POST['heuresDispo']))
+  {
+    //erreur au niveau du message
+    $erreur= true;
+  }
 if(!$erreur)
 {
 	$nom=$_POST['nom'];
 	$prenom=$_POST['prenom'];
 	$club=$_POST['club'];
-	$req=$bdd->prepare('INSERT INTO infoperso(nom,prenom,club) VALUES (:nom,:prenom,:club)');
+  $poste1=$_POST['poste1'];
+  $poste2=$_POST['poste2'];
+  $poste3=$_POST['poste3'];
+  $joursDispo=$_POST['joursDispo'];
+  $heuresDispo=$_POST['heuresDispo'];
+	$req=$bdd->prepare('INSERT INTO infoperso(nom,prenom,club,poste1,poste2,poste3,joursDispo,heuresDispo) VALUES (:nom,:prenom,:club,:poste1,:poste2,:poste3,:joursDispo,:heuresDispo)');
 	$req->execute(array(
 	'nom'=>$nom,
 	'prenom'=>$prenom,
 	'club'=>$club,
-	));
-	$rep=$bdd->query('SELECT * FROM infoperso');
-	$donnees=$rep->fetch();
+  'poste1'=>$poste1,
+  'poste2'=>$poste2,
+  'poste3'=>$poste3,
+  'joursDispo'=>$joursDispo,
+  'heuresDispo'=>$heuresDispo,
+
+  ));
+  echo "Okay";
+	
+	// $rep=$bdd->query('SELECT * FROM infoperso');
+	// $donnees=$rep->fetch();
 }
 else{
 	echo 'remplie les champ poto';
